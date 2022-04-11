c4e6dd46fb3695c2dbd0f914f68a3ca5ebdb2b8a
Julian Barbosa <bjvlixn@gmail.com>
2022-04-11 00:42:14 -0300

    add request validation

--- backend/src/app/controllers/LocationController.js
+++ backend/src/app/controllers/LocationController.js
@@ -1,13 +1,21 @@
-const LocationRepository = require("../repositories/LocationRepository");
 const GoogleGeolocationService = require("../services/GoogleGeolocationService");
 
 class LocationController {
   async index(request, response) {
     const { locations } = request.params;
-    const locationList = locations.split(",");
 
-    GoogleGeolocationService.getLocation(locations);
-    response.json({ locationList });
+    if (!locations) {
+      return response.status(400).json({ error: "Locations are required" });
+    }
+    if (locations.split(";").length < 1) {
+      return response
+        .status(400)
+        .json({ error: "At least two locations are required" });
+    }
+
+    const data = await GoogleGeolocationService.getLocation(locations);
+
+    response.json({ data });
   }
 }
 
